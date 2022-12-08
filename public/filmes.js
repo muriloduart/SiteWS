789b41ec44f8762dabf69bc7eaac19138956b4e9
muriloduart <muriloduarte@outlook.com>
2022-12-08 16:17:56 -0400

    Montando Cards da Aba Filmes

--- public/filmes.js
+++ public/filmes.js
@@ -1,22 +1,29 @@
-const client = axios.create({
-  baseURL: "https://swapi.py4e.com/api/films/1/" 
-});
+
+  
+/*const url = "https://swapi.py4e.com/api/films/1/"
 
 
+function getFilm() {
+    var div_film_info = document.getElementById('film-info');
+    
 
-const App = () => {
-    const [posts, setPosts] = useState([]);
- 
-    useEffect(() => {
-       client.get('?_limit=10').then((response) => {
-          setPosts(response.data);
-          console.log(data);
-       });
-    }, []);
- 
-    return (
-       console.error(error)
-    );
- };
- 
- export default App;
+    axios.get(url)
+    .then(response =>{
+        console.log(response);
+        const data = response.data.title;
+        div_film_info.innerHTML = JSON.stringify(data);
+        console.log(data);
+        /* let template = '<div>';
+        template += '<div class="film-info">';
+        template += '';
+        template+= '</div>'; 
+        div_film_info.innerHTML = template;*/
+
+
+/*
+    })
+    .catch(error => console.log(error))
+}
+
+getFilm();
+*/
